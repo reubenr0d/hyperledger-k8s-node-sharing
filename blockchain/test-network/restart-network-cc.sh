2b8790bf544421b7811eb386ff397041d5a9b098
Reuben Rodrigues <reuben.r@solutionbeyond.net>
2020-11-27 04:27:22 -0800

    Added  veridication on peers for adding slice

--- blockchain/test-network/restart-network-cc.sh
+++ blockchain/test-network/restart-network-cc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo ./network.sh down
 sudo ./network.sh up createChannel -ca
-sudo ./network.sh deployCC -ccn sinode -ccp ../chaincode -ccl go
+sudo ./network.sh deployCC -ccn sinode -ccp ../chaincode -ccl go -ccep "AND('Org1MSP.peer','Org2MSP.peer')"
 cd ../test-app
 sudo rm -rf wallet
 sudo rm -rf keystore
